c1cbd64d56c01ef8ad866e741762d220df21c556
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2024-11-02 23:02:53 -0700

    clean up imports

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -3,9 +3,6 @@ import bullsEye from "../assets/bulls-eye.webp";
 import thumbsUp from "../assets/thumbs-up.webp";
 import meh from "../assets/meh.webp";
 
-import React from "react";
-import { BiBullseye } from "react-icons/bi";
-
 interface Props {
   rating: number;
 }
