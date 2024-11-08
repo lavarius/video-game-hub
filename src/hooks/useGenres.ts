d8f415d6bf83e68cfc64a5362b970a818446f6eb
Mark Bartolo <6192527+lavarius@users.noreply.github.com>
2024-11-08 15:13:43 -0800

    Fetch platforms and implement with initialData

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -9,8 +9,6 @@ export interface Genre {
   image_background: string;
 }
 
-// const useGenres = () => ({ data: genres, isLoading: false, error: null });
-
 const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
